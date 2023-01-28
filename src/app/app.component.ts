a2d8bdc81dedd4288102f1eeaf5024327363e085
nikhildhar1992 <nikhildhar92@gmail.com>
2023-01-28 22:15:16 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   public activeBar = '';
   menu = {
     home: 'Home',
     history: 'History',
-  }
-  constructor(){}
+  };
+
+  constructor() {}
 }
